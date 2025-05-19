b79b90f25d372c9aec70ad43b4e3cf0e960c9580
gyj <gyj@gmail.com>
2025-05-19 12:38:22 +0900

    젠킨스 자동 배포 테스트5

--- src/main/java/com/lms/api/admin/TestController.java
+++ src/main/java/com/lms/api/admin/TestController.java
@@ -7,8 +7,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/test")
 public class TestController {
-    @GetMapping
+    @GetMapping("/1")
     public String test1(){
         return "자동 배포 파일 내용 변경 확인 완료";
+    }
+
+    @GetMapping("/2")
+    public String test2(){
+        return "";
     }
 }
