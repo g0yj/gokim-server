85c11758d7993c77a908695d10f08558c48c823b
gyj <gyj@gmail.com>
2025-05-21 09:21:24 +0900

    초기값 셋팅

--- src/main/java/com/lms/api/common/mock/TestDataInitializer.java
+++ src/main/java/com/lms/api/common/mock/TestDataInitializer.java
@@ -1,11 +1,14 @@
 package com.lms.api.common.mock;
 
 import com.lms.api.admin.auth.enums.LoginType;
+import com.lms.api.admin.project.enums.ProjectFunctionType;
 import com.lms.api.admin.project.enums.ProjectRole;
 import com.lms.api.admin.user.enums.UserRole;
 import com.lms.api.common.entity.UserEntity;
+import com.lms.api.common.entity.id.ProjectMemberId;
 import com.lms.api.common.entity.project.FunctionEntity;
 import com.lms.api.common.entity.project.ProjectEntity;
+import com.lms.api.common.entity.project.ProjectFunctionEntity;
 import com.lms.api.common.entity.project.ProjectMemberEntity;
 import com.lms.api.common.entity.project.task.SubTaskEntity;
 import com.lms.api.common.entity.project.task.TaskCommentEntity;
@@ -13,6 +16,7 @@ import com.lms.api.common.entity.project.task.TaskEntity;
 import com.lms.api.common.entity.project.task.TaskStatusEntity;
 import com.lms.api.common.repository.UserRepository;
 import com.lms.api.common.repository.project.FunctionRepository;
+import com.lms.api.common.repository.project.ProjectFunctionRepository;
 import com.lms.api.common.repository.project.ProjectMemberRepository;
 import com.lms.api.common.repository.project.ProjectRepository;
 import com.lms.api.common.repository.project.task.SubTaskRepository;
@@ -24,7 +28,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -40,147 +46,200 @@ public class TestDataInitializer implements CommandLineRunner {
     private final SubTaskRepository subTaskRepository;
     private final TaskCommentRepository taskCommentRepository;
     private final PasswordEncoder passwordEncoder;
+    private final ProjectFunctionRepository projectFunctionRepository;
     private final FunctionRepository functionRepository;
 
     @Override
+    @Transactional
     public void run(String... args) throws Exception {
         log.debug("샘플 데이터 삽입 시작 ..");
 
-        // 유저 생성
-        UserEntity owner = createUserIfNotExists("SampleOwner", "1234", "프로젝트소유자", "duswls3000@gmail.com", "01091901376", UserRole.USER, LoginType.NORMAL);
-        UserEntity member1 = createUserIfNotExists("SampleMember1", "1234", "프로젝트멤버1", "jinyjgo@naver.com", "01079168787", UserRole.USER, LoginType.NORMAL);
-        UserEntity member2 = createUserIfNotExists("SampleMember2", "1234", "프로젝트멤버2", null, null, UserRole.USER, LoginType.NORMAL);
-        UserEntity member3 = createUserIfNotExists("SampleMember3", "1234", "프로젝트멤버3", null, null, UserRole.USER, LoginType.NORMAL);
-        UserEntity admin = createUserIfNotExists("Admin","1234", "관리자", "goyeonjin@naver.com", null, UserRole.ADMIN, LoginType.NORMAL);
+        // 기능(functionTask,functionCalendar,functionBoard,functionFile)
+        FunctionEntity functionTask = createFunctionIfNotExists(ProjectFunctionType.TASK, "보드");
+        FunctionEntity functionCalendar = createFunctionIfNotExists(ProjectFunctionType.CALENDAR, "캘린더");
+        FunctionEntity functionBoard = createFunctionIfNotExists(ProjectFunctionType.BOARD, "게시판");
+        FunctionEntity functionFile = createFunctionIfNotExists(ProjectFunctionType.FILE, "첨부파일");
+        // 회원(owner,member1,member2,member3, admin)
+        UserEntity user1 = createUserIfNotExists("SampleOwner", "1234", "프로젝트소유자", "duswls3000@gmail.com", "01091901376", UserRole.USER, LoginType.NORMAL);
+        UserEntity user2 = createUserIfNotExists("SampleMember1", "1234", "프로젝트멤버1", "jinyjgo@naver.com", "01079168787", UserRole.USER, LoginType.NORMAL);
+        UserEntity user3 = createUserIfNotExists("SampleMember2", "1234", "프로젝트멤버2", null, null, UserRole.USER, LoginType.NORMAL);
+        UserEntity user4 = createUserIfNotExists("SampleMember3", "1234", "프로젝트멤버3", null, null, UserRole.USER, LoginType.NORMAL);
+        UserEntity admin = createUserIfNotExists("Admin", "1234", "관리자", "goyeonjin@naver.com", null, UserRole.ADMIN, LoginType.NORMAL);
+        // 프로젝트(project)
+        ProjectEntity project = createProjectIfNotExists("P1234", "샘플 프로젝트", user1);
+        // 프로젝트 멤버(owner, member1, member2, member3)
+        ProjectMemberEntity owner = createProjectMemberIfNotExists(project, user1, ProjectRole.OWNER);
+        ProjectMemberEntity member1 = createProjectMemberIfNotExists(project, user2, ProjectRole.MEMBER);
+        ProjectMemberEntity member2 = createProjectMemberIfNotExists(project, user3, ProjectRole.MEMBER);
+        ProjectMemberEntity member3 = createProjectMemberIfNotExists(project, user4, ProjectRole.MEMBER);
+        // 프로젝트 기능 (board ,task, calendar)
+        ProjectFunctionEntity board = createProjectFunctionIfNotExists("PF01", "게시판11", functionBoard.getProjectFunctionType(), 1L, project);
+        ProjectFunctionEntity task = createProjectFunctionIfNotExists("PF02", "보드11", functionTask.getProjectFunctionType(), 2L, project);
+        ProjectFunctionEntity calendar = createProjectFunctionIfNotExists("PF03", "캘린더11", functionCalendar.getProjectFunctionType(), 3L, project);
+        // task 상태(task) : 기본값 먼저 있으면 실행
+        TaskStatusEntity idea = createTaskStatusIfNotExists("Idea", project);
+        TaskStatusEntity todo = createTaskStatusIfNotExists("Todo", project);
+        TaskStatusEntity inProgress = createTaskStatusIfNotExists("InProgress", project);
+        TaskStatusEntity done = createTaskStatusIfNotExists("Done", project);
+        // task 추가
+        TaskEntity task1 = createTaskIfNotExists("T1111", "테이블 설계", "테이블 설계에 관련된 내용입니다. 추후 에디터 사용", 1, owner.getUserEntity().getId(), board, idea, owner.getUserEntity().getId());
+        TaskEntity task2 = createTaskIfNotExists("T2222", "추가 기획", "챗봇 추가 계획", 2, owner.getUserEntity().getId(), board, todo, owner.getUserEntity().getId());
+        TaskEntity task3 = createTaskIfNotExists("T3333", "리액트 api와 연결", "현재 mock 데이터 사용 중", 3, member1.getUserEntity().getId(), board, idea, owner.getUserEntity().getId());
+        TaskEntity task4 = createTaskIfNotExists("T4444", "회의록 정리", "별도로 캘린더를 만들어서 운영", 4, member2.getUserEntity().getId(), board, inProgress, owner.getUserEntity().getId());
+        TaskEntity task5 = createTaskIfNotExists("T5555", "순서 변환 라이브러리 사용", "설명해주기", 5, member3.getUserEntity().getId(), board, done, owner.getUserEntity().getId());
 
-        // 프로젝트 생성
-        ProjectEntity project = ProjectEntity.builder()
-                .id("P1234")
-                .projectName("샘플 프로젝트")
-                .userEntity(owner)
-                .createdBy(owner.getId())
-                .build();
-        projectRepository.save(project);
+        // 하위 항목
+        createSubTaskIfNotExists("첫번째 하위 항목입니다.", task1, idea, member1);
+        createSubTaskIfNotExists("두번째 하위 항목입니다.", task1, done, member2);
+        createSubTaskIfNotExists("세번째 하위 항목입니다.", task1, done, member2);
+        // 댓글
+        createTaskCommentIfNotExists("첫번째 댓글!!", task1, member2.getUserEntity().getId());
+        createTaskCommentIfNotExists("두번째 댓글!!", task1, member2.getUserEntity().getId());
+        createTaskCommentIfNotExists("두번째 댓글!!", task1, member3.getUserEntity().getId());
 
-        // 프로젝트 초대
-        ProjectMemberEntity assignee = addProjectMember(project, owner, ProjectRole.OWNER);
-        addProjectMember(project, member1, ProjectRole.MEMBER);
-        addProjectMember(project, member2, ProjectRole.MEMBER);
-
-        // 기능 추가
-        FunctionEntity dashboard = addFunction("PF01","대시보드", 1L , project);
-        FunctionEntity board = addFunction("PF02", "할일", 2L, project);
-        FunctionEntity calendar = addFunction("PF03", "캘린더", 3L, project);
-
-        // 프로젝트 할일 등록
-        List<String> statusNames = Arrays.asList("Idea", "Todo", "InProgress", "Done");
-        statusNames.stream()
-                .map(name -> TaskStatusEntity.builder()
-                        .projectEntity(project)
-                        .name(name)
-                        .build())
-                .forEach(taskStatusRepository::save);
-
-        // 등록된 TaskStatusEntity를 사용하여 Task 추가
-        TaskStatusEntity idea = taskStatusRepository.findByNameAndProjectEntity_Id("Idea", project.getId());
-        TaskStatusEntity todo = taskStatusRepository.findByNameAndProjectEntity_Id("Todo", project.getId());
-        TaskStatusEntity inProgress = taskStatusRepository.findByNameAndProjectEntity_Id("InProgress", project.getId());
-        TaskStatusEntity done = taskStatusRepository.findByNameAndProjectEntity_Id("Done", project.getId());
-
-        TaskEntity task = addTask("T1111", "테이블 설계", "테이블 설계에 관련된 내용입니다. 추후 에디터 사용", 1, board, owner, idea);
-        addTask("T2222", "프로젝트 관련 API 생성", "CRUD에 대한 API 설명을 추가해주세요", 2, board, owner, todo);
-        addTask("T3333", "보드 관련 API 생성", "멤버가 등록한 TODO입니다", 3, board, member1, inProgress);
-        addTask("T4444", "수정 사항 확인", "수정된 내용 체크했으면 문자로 알려주세요", 4, board, member1, done);
-
-        addSubTask("첫번째 하위 항목입니다." ,task, idea, assignee);
-        addSubTask("두번째 하위 항목입니다." ,task, done, assignee);
-
-        addTaskComment("첫번째 댓글!!",task, member2.getId());
-        addTaskComment("두번째 댓글!!",task, owner.getId());
 
         log.debug("샘플 데이터 삽입 완료");
     }
 
-    /**
-     * 유저가 존재하지 않으면 새로운 유저를 추가하는 메서드
-     */
-    private UserEntity createUserIfNotExists(String id, String rawPassword, String name, String email, String phone, UserRole role , LoginType loginType) {
-        return userRepository.findById(id).orElseGet(() -> {
-            String encodedPassword = passwordEncoder.encode(rawPassword);
-            UserEntity user = UserEntity.builder()
-                    .id(id)
-                    .password(encodedPassword)
-                    .name(name)
-                    .email(email)
-                    .phone(phone)
-                    .role(role)
-                    .loginType(loginType)
-                    .build();
-            return userRepository.save(user);
-        });
+    @Transactional
+    private FunctionEntity createFunctionIfNotExists(ProjectFunctionType type, String name) {
+        return functionRepository.findById(type)
+                .orElseGet(() -> {
+                    FunctionEntity function = FunctionEntity.builder()
+                            .projectFunctionType(type)
+                            .functionName(name)
+                            .build();
+                    return functionRepository.save(function);
+                });
+    }
+    @Transactional
+    private UserEntity createUserIfNotExists(String id, String rawPassword, String name, String email, String phone, UserRole role, LoginType loginType) {
+        return userRepository.findById(id)
+                .orElseGet(() -> {
+                    String encodedPassword = passwordEncoder.encode(rawPassword);
+                    UserEntity user = UserEntity.builder()
+                            .id(id)
+                            .password(encodedPassword)
+                            .name(name)
+                            .email(email)
+                            .phone(phone)
+                            .role(role)
+                            .loginType(loginType)
+                            .build();
+                    return userRepository.save(user);
+                });
     }
 
-    /**
-     * 프로젝트 멤버를 추가하는 메서드
-     */
-    private ProjectMemberEntity addProjectMember(ProjectEntity project, UserEntity user, ProjectRole projectRole) {
-        ProjectMemberEntity member = ProjectMemberEntity.builder()
-                .projectId(project.getId())
-                .projectMemberId(user.getId())
-                .projectRole(projectRole)
-                .projectEntity(project)
-                .userEntity(user)
-                .build();
-        projectMemberRepository.save(member);
-        return member;
+    @Transactional
+    private ProjectEntity createProjectIfNotExists(String projectId, String projectName, UserEntity owner) {
+        return projectRepository.findById(projectId)
+                .orElseGet(() -> {
+                    ProjectEntity project = ProjectEntity.builder()
+                            .id(projectId)               // ID 직접 세팅 필수
+                            .projectName(projectName)
+                            .userEntity(owner)
+                            .build();
+                    return projectRepository.save(project);
+                });
     }
 
-    private FunctionEntity addFunction(String id, String functionName, Long functionSort , ProjectEntity projectEntity){
-        FunctionEntity function = FunctionEntity.builder()
-                .id(id)
-                .functionName(functionName)
-                .functionSort(functionSort)
-                .projectEntity(projectEntity)
-                .build();
-        functionRepository.save(function);
-        return function;
+    @Transactional
+    private ProjectMemberEntity createProjectMemberIfNotExists(ProjectEntity project, UserEntity user, ProjectRole role) {
+        ProjectMemberId id = new ProjectMemberId();
+        id.setProjectId(project.getId());
+        id.setProjectMemberId(user.getId());
+
+        return projectMemberRepository.findById(id)
+                .orElseGet(() -> {
+                    ProjectMemberEntity member = ProjectMemberEntity.builder()
+                            .projectId(project.getId())
+                            .projectMemberId(user.getId())
+                            .projectRole(role)
+                            .projectEntity(project)
+                            .userEntity(user)
+                            .build();
+                    return projectMemberRepository.save(member);
+                });
+    }
+    @Transactional
+    private TaskStatusEntity createTaskStatusIfNotExists(String name, ProjectEntity project) {
+        return taskStatusRepository.findByNameAndProjectEntity_Id(name, project.getId())
+                .stream()
+                .findFirst()
+                .orElseGet(() -> {
+                    TaskStatusEntity status = TaskStatusEntity.builder()
+                            .name(name)
+                            .projectEntity(project)
+                            .build();
+                    return taskStatusRepository.save(status);
+                });
     }
 
-    /**
-     * Task를 추가하는 메서드
-     */
-    private TaskEntity addTask(String id, String title, String content, int sortOrder, FunctionEntity functionEntity, UserEntity userEntity, TaskStatusEntity taskStatusEntity ) {
-        TaskEntity task = TaskEntity.builder()
-                .id(id)
-                .title(title)
-                .description(content)
-                .sortOrder(sortOrder)
-                .assignedMember(userEntity.getId())
-                .functionEntity(functionEntity)
-                .taskStatusEntity(taskStatusEntity)
-                .createdBy(userEntity.getId())
-                .build();
-        taskRepository.save(task);
-        return task;
+
+    @Transactional
+    private ProjectFunctionEntity createProjectFunctionIfNotExists(
+            String id, String functionName, ProjectFunctionType functionType, Long sort, ProjectEntity project) {
+
+        return projectFunctionRepository.findById(id)
+                .stream()
+                .findFirst()
+                .orElseGet(() -> {
+                    ProjectFunctionEntity projectFunction = ProjectFunctionEntity.builder()
+                            .id(id)
+                            .projectFunctionName(functionName)
+                            .projectFunctionSort(sort)
+                            .projectFunctionType(functionType)
+                            .projectEntity(project)
+                            .build();
+                    return projectFunctionRepository.save(projectFunction);
+                });
     }
 
-    private void addSubTask(String content, TaskEntity taskEntity, TaskStatusEntity taskStatusEntity, ProjectMemberEntity assignee  ) {
-        SubTaskEntity subTask = SubTaskEntity.builder()
-                .content(content)
-                .taskEntity(taskEntity)
-                .taskStatusEntity(taskStatusEntity)
-                .assignee(assignee)
-                .build();
-        subTaskRepository.save(subTask);
-    }
 
-    private void addTaskComment(String content, TaskEntity taskEntity , String userEntity){
-        TaskCommentEntity taskComment = TaskCommentEntity.builder()
-                .content(content)
-                .taskEntity(taskEntity)
-                .createdBy(userEntity)
-                .build();
-        taskCommentRepository.save(taskComment);
-    }
 
+
+    @Transactional
+    private TaskEntity createTaskIfNotExists(String id, String title, String description, int sortOrder, String assignedMember,
+                                             ProjectFunctionEntity projectFunction, TaskStatusEntity taskStatus, String createdBy) {
+        return taskRepository.findById(id)
+                .orElseGet(() -> {
+                    TaskEntity task = TaskEntity.builder()
+                            .id(id)
+                            .title(title)
+                            .description(description)
+                            .sortOrder(sortOrder)
+                            .assignedMember(assignedMember)
+                            .projectFunctionEntity(projectFunction)
+                            .taskStatusEntity(taskStatus)
+                            .createdBy(createdBy)
+                            .build();
+                    return taskRepository.save(task);
+                });
+    }
+    @Transactional
+    private SubTaskEntity createSubTaskIfNotExists(String content, TaskEntity taskEntity,
+                                                   TaskStatusEntity taskStatusEntity, ProjectMemberEntity projectMemberEntity) {
+        return subTaskRepository.findByTaskEntityAndContentAndAssignee(taskEntity, content, projectMemberEntity)
+                .orElseGet(() -> {
+                    SubTaskEntity subTask = SubTaskEntity.builder()
+                            .content(content)
+                            .taskEntity(taskEntity)
+                            .taskStatusEntity(taskStatusEntity)
+                            .assignee(projectMemberEntity)
+                            .build();
+                    return subTaskRepository.save(subTask);
+                });
+    }
+    @Transactional
+    private TaskCommentEntity createTaskCommentIfNotExists(String content, TaskEntity taskEntity, String modifiedBy) {
+        return taskCommentRepository.findByTaskEntityAndContent(taskEntity, content)
+                .orElseGet(() -> {
+                    TaskCommentEntity comment = TaskCommentEntity.builder()
+                            .content(content)
+                            .taskEntity(taskEntity)
+                            .modifiedBy(modifiedBy)
+                            .build();
+                    return taskCommentRepository.save(comment);
+                });
+    }
 }
