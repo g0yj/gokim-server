b5d1cba1f09042e776d0f8ebcef944c7f2dde77e
gyj <gyj@gmail.com>
2025-06-10 16:38:52 +0900

    페이징 처리시 no 계산을 위해 공통 클래스 수정

--- src/main/java/com/lms/api/common/dto/PageResponse.java
+++ src/main/java/com/lms/api/common/dto/PageResponse.java
@@ -1,45 +1,49 @@
 package com.lms.api.common.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import java.util.List;
-
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.experimental.FieldDefaults;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
 
 @Getter
-@AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @Schema(name = "PageResponse", description = "페이징 처리 정보")
 public class PageResponse<T> {
 
-  @Schema(description = "페이징 처리에 따른 no 값")
+  @Schema(description = "결과 리스트")
   List<T> list;
+
   @Schema(description = "총 게시물 수")
   long totalCount;
-  @Schema(description = "현재 페이지")
+
+  @Schema(description = "현재 페이지 번호")
   int page;
-  @Schema(description = "한 페이지에서 보여줄 게시글 수")
+
+  @Schema(description = "한 페이지당 게시물 수")
   int limit;
-  @Schema(description = "페이지 바 갯수")
+
+  @Schema(description = "페이지 바 크기")
   int pageSize;
 
-  public List<T> getList() {
-    if (list == null) {
-      return List.of();
-    }
+  public PageResponse(Page<T> page, int pageSize) {
+    this.list = page.getContent();
+    this.totalCount = page.getTotalElements();
+    int currentPage = page.getNumber() + 1;
+    this.page = currentPage;
+    this.limit = page.getSize();
+    this.pageSize = pageSize;
 
-    if (list.isEmpty() || !(list.get(0) instanceof PageResponseData)) {
-      return list;
+    if (list != null && !list.isEmpty() && list.get(0) instanceof PageResponseData) {
+      for (int i = 0; i < list.size(); i++) {
+        ((PageResponseData) list.get(i)).setListNumber(
+                totalCount - (long) (currentPage - 1) * limit - i
+        );
+      }
     }
-
-    for (int i = 0; i < list.size(); i++) {
-      ((PageResponseData) list.get(i)).setListNumber(totalCount - (long) (page - 1) * limit - i);
-    }
-
-    return list;
   }
 
   public int getTotalPage() {
--- src/main/java/com/lms/api/common/dto/PageResponseData.java
+++ src/main/java/com/lms/api/common/dto/PageResponseData.java
@@ -1,11 +1,12 @@
 package com.lms.api.common.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public abstract class PageResponseData {
-
+  @Schema(description = "no")
   private long listNumber;
 }
